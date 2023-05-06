f0160ecbc0fcd087f4e8b72cc2b2cf5ee17e4e38
Vanessa Santos <114835013+VanessaPro@users.noreply.github.com>
2023-05-06 16:40:02 -0300

    testando o javascript

--- code.js
+++ code.js
@@ -1,15 +1,24 @@
-const input = document.querySelector("input");
-const qrcode = document.querySelector("qrcode")
+const wrapper =document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn =wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
 
-document.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-      canQRCode();
+generateBtn.addEventListener("click", () => {
+
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    generateBtn.innerText = "Gerando Qr Code..."
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        generateBtn.innerText = "Gerar Qr Code"
+        wrapper.classList.add("active");
+    });
+   
+});
+
+qrInput.addEventListener("keyup", () => {
+
+    if(!qrInput.value){
+        wrapper.classList.remove("active");
     }
-  })
-
-  function canQRCode() {
-    if (!input.value) return;
-  
-    
-  }
-
+});
